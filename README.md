edfe4935eab1cc08de090c56b4b274f336f0cbeb
alan au <aulun0@yahoo.com.hk>
2022-03-05 14:14:32 +0800

    more examples

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 2. open cmd and run "node main.js"
 3. Open browser and "http://127.0.0.1:8081/"
 
-a
+1. open cmd and run "node server.js"
+2. Open browser and "http://127.0.0.1:8081/index.html"
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var http = require("http");
 
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+
 http.createServer(function (request, response) {
    // Send the HTTP header 
    // HTTP Status: 200 : OK
@@ -12,3 +15,13 @@ http.createServer(function (request, response) {
 
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
+
+// non-blocking code
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+
+// blocking code
+//console.log(data.toString());
+console.log("Program Ended");
